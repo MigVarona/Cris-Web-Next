2e957cafb53480eef3de39873cb2a2ba7a81fa57
MigVarona <migvaronag@gmail.com>
2024-06-01 17:57:23 +0200

    Primer commit Nextjs Cris web

---
+++ src/app/components/Hero.jsx
@@ -0,0 +1,52 @@
+import "../../app/globals.css";
+
+
+function Hero() {
+    return ( 
+
+        <div className="home-banner-section">
+  <div className="hero-1">
+    <div data-w-id="dade728e-c94b-1751-0e9e-0bff99516276" className="overflow">
+      <div data-w-id="1389cbe7-036f-41ac-6a89-5caf56b8be7f" style={{ opacity: 0 }} className="heading-banner-1">
+        Cristina
+      </div>
+    </div>
+    <div data-w-id="14703866-07cc-c290-1e70-2760606a46f9" className="overflow">
+      <h1 data-w-id="14703866-07cc-c290-1e70-2760606a46fa" style={{ opacity: 0 }} className="heading-banner-1">
+        Gomez
+      </h1>
+    </div>
+  </div>
+  <div className="hero-multiply">
+    <div className="overflow">
+      <div data-w-id="9dd5b4e6-c80a-f45c-cbb4-685676604bfd" style={{ opacity: 0 }} className="heading-banner-1 multiply">
+        Video editor
+      </div>
+    </div>
+  </div>
+  <div className="hero-project">
+    <div className="collection-list-wrapper w-dyn-list">
+      <div role="list" className="collection-list w-dyn-items">
+        <div role="listitem" className="collection-item-2 w-dyn-item">
+          <a className="link-hero w-inline-block">
+            <div className="bg-wrap">
+              <div data-autoplay="true" data-loop="true" className="background-video w-background-video w-background-video-atom">
+                <video autoPlay loop muted playsInline data-object-fit="cover">
+                  <source src="/loop entrada.mp4" type="video/mp4" />
+                </video>
+                <video autoPlay loop muted playsInline data-object-fit="cover">
+                  <source src="/LOOP movil.mp4" type="video/mp4" />
+                </video>
+              </div>
+            </div>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+     );
+}
+
+export default Hero;
--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,167 @@
 import Image from "next/image";
+import Hero from "./components/Hero"
+import Script from 'next/script'
+
+
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main>
+   
+    <div>
+   
+        <div
+          data-animation="over-left"
+          className="navbar-transparent w-nav"
+          data-easing2="ease"
+          data-easing="ease"
+          data-collapse="medium"
+          role="banner"
+          data-no-scroll="1"
+          data-duration="400"
+          data-doc-height="1"
+        >
+          <div className="gradient"></div>
+          <div className="nav-container w-container">
+            <div className="nav-menu-wrapper">
+              <a href="index.html" className="brand w-nav-brand">
+                <Image
+                  src="/flaticon home.png"
+                  alt="logo"
+                  width={80}
+                  height={80}
+                  loading="lazy"
+                />
+              </a>
+              <nav role="navigation" className="nav-menu w-nav-menu">
+                <div className="tablet-menu">
+                  <a href="index.html" className="brand-tablet w-nav-brand">
+                    <Image
+                      src="/flaticon home.png"
+                      alt="logo"
+                      width={40}
+                      height={40}
+                      loading="lazy"
+                    />
+                  </a>
+                  <div className="close-menu-button w-nav-button">
+                    <Image
+                      src="/flaticon home.png"
+                      alt="icon"
+                      height={16}
+                      width={16}
+                      loading="lazy"
+                      className="nav-close-icon"
+                    />
+                  </div>
+                </div>
+                <div className="menu-wrap">
+                  <div
+                    data-hover="true"
+                    data-delay="0"
+                    data-w-id="1bba802f-7b9e-f63d-4f08-053960cc3754"
+                    className="nav-dropdown w-dropdown"
+                  >
+                    <div className="nav-dropdown-toggle w-dropdown-toggle">
+                      <div className="clip">
+                        <div className="btn-banner-text">
+                          <a href="about.html" style={{ color: "white" }}>
+                            <div className="btn-title-text">
+                              About Me <span className="tablet-hidden">+</span>
+                            </div>
+                          </a>
+                        </div>
+                        <div className="btn-banner-text button-text-bottom">
+                          <div className="btn-title-text">About Me +</div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div
+                    data-hover="true"
+                    data-delay="0"
+                    data-w-id="1bba802f-7b9e-f63d-4f08-053960cc377a"
+                    className="nav-dropdown w-dropdown"
+                  >
+                    <div className="nav-dropdown-toggle w-dropdown-toggle">
+                      <div className="nav-dropdown-icon w-icon-dropdown-toggle"></div>
+                      <div className="clip">
+                        <div className="btn-banner-text">
+                          <div className="btn-title-text">
+                            Works <span className="tablet-hidden">+</span>
+                          </div>
+                        </div>
+                        <div className="btn-banner-text button-text-bottom">
+                          <div className="btn-title-text">Works +</div>
+                        </div>
+                      </div>
+                    </div>
+                    <nav className="nav-dropdown-list w-dropdown-list">
+                      <div className="nav-dropdown-link-wrapper">
+                        <a href="education.html" className="nav-dropdown-link w-dropdown-link">
+                          <span className="nav-dropdown-link-line"></span>Education
+                        </a>
+                        <a href="entertaiment.html" className="nav-dropdown-link w-dropdown-link">
+                          <span className="nav-dropdown-link-line"></span>Entertaiment
+                        </a>
+                        <a href="corporate.html" className="nav-dropdown-link w-dropdown-link">
+                          <span className="nav-dropdown-link-line"></span>Corporate
+                        </a>
+                      </div>
+                    </nav>
+                  </div>
+                  <a data-w-id="1bba802f-7b9e-f63d-4f08-053960cc379e" className="nav-link w-inline-block">
+                    <div className="clip">
+                      <div className="btn-banner-text">
+                        <div className="btn-title-text"></div>
+                      </div>
+                      <div className="btn-banner-text button-text-bottom">
+                        <div className="btn-title-text"></div>
+                      </div>
+                    </div>
+                  </a>
+                  <div className="nav-button-wrapper">
+                    <a data-w-id="1bba802f-7b9e-f63d-4f08-053960cc380e" href="contact.html" className="nav-button w-inline-block">
+                      <div className="clip">
+                        <div className="btn-banner-text">
+                          <div className="btn-title-text">Let&#x27;s Talk</div>
+                        </div>
+                        <div className="btn-banner-text button-text-bottom">
+                          <div className="btn-title-text">Let&#x27;s Talk</div>
+                        </div>
+                      </div>
+                    </a>
+                  </div>
+                </div>
+              </nav>
+              <div className="menu-button w-nav-button">
+                <Image
+                  src="https://assets-global.website-files.com/66095c4f9765fb9eae51e781/66095c4f9765fb9eae51e859_menu-btn.svg"
+                  alt="icon"
+                  width={16}
+                  height={16}
+                  loading="lazy"
+                  className="image-burger"
+                />
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+     
+    </div>
+    <Hero />
+    <script
+    src="https://code.jquery.com/jquery-3.5.1.min.js"
+    integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
+    crossOrigin="anonymous"
+  ></script>
+  <Script src="/main.js" strategy="lazyOnload" />
+   
     </main>
+ 
+   
+
+
+    
   );
 }
